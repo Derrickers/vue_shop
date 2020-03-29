ca24c042a1f6a9f30481c00a33ae5d0d2f305d38
Enhao Xu <37367863+Derrickers@users.noreply.github.com>
2020-03-29 16:05:26 +0800

    login complete

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="login_container">
         <div class="login_box">
-<!--            头像区-->
+            <!--            头像区-->
             <div class="avatar_box">
                 <img src="../assets/logo.png">
             </div>
-<!--            登陆表单区-->
+            <!--            登陆表单区-->
             <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
-<!--            用户名输入-->
+                <!--            用户名输入-->
                 <el-form-item prop="username">
-                    <el-input placeholder="请输入用户名" v-model="loginForm.username" prefix-icon="el-icon-user-solid"></el-input>
+                    <el-input placeholder="请输入用户名" v-model="loginForm.username"
+                              prefix-icon="el-icon-user-solid"></el-input>
                 </el-form-item>
-<!--            密码输入-->
+                <!--            密码输入-->
                 <el-form-item prop="password">
-                    <el-input placeholder="请输入密码" type="password" v-model="loginForm.password" prefix-icon="el-icon-lock"></el-input>
+                    <el-input placeholder="请输入密码" type="password" v-model="loginForm.password"
+                              prefix-icon="el-icon-lock"></el-input>
                 </el-form-item>
-<!--            按钮区-->
+                <!--            按钮区-->
                 <el-form-item class="btns">
                     <el-button type="primary" @click="login">登陆</el-button>
                     <el-button type="info" @click="resetLoginForm">重置</el-button>
@@ -27,41 +29,42 @@
 
 <script>
     export default {
-        data(){
-            return{
-                loginForm:{
-                    username:"admin",
-                    password:"123456",
+        data() {
+            return {
+                loginForm: {
+                    username: "admin",
+                    password: "123456",
                 },
-                loginFormRules:{
+                loginFormRules: {
                     //验证用户名是否合法
-                    username:[
-                        {required:true,message:"请输入用户名",trigger:"blur"},
-                        {min:3,max:10,message: "长度在3到10个字符之间",trigger: "blur"}
+                    username: [
+                        {required: true, message: "请输入用户名", trigger: "blur"},
+                        {min: 3, max: 10, message: "长度在3到10个字符之间", trigger: "blur"}
                     ],
                     //验证用户密码是否合法
-                    password:[
-                        {required:true,message:"请输入密码",trigger:"blur"},
-                        {min:6,max:22,message: "长度在6到22个字符之间",trigger: "blur"}
+                    password: [
+                        {required: true, message: "请输入密码", trigger: "blur"},
+                        {min: 6, max: 22, message: "长度在6到22个字符之间", trigger: "blur"}
                     ]
 
                 }
             }
         },
-        methods:{
-            resetLoginForm:function(){
+        methods: {
+            resetLoginForm: function () {
                 this.$refs.loginFormRef.resetFields();
             },
-            login:function () {
+            login: function () {
                 this.$refs.loginFormRef.validate(async valid => {
-                    if(!valid)
+                    if (!valid)
                         return;
                     console.log(this.loginForm)
-                    const {data:res} = await this.$http.post('user/login',this.loginForm)
-                    console.log(res)
-                    if(res.meta.code!==200)
+                    const {data: res} = await this.$http.post('user/login', this.loginForm)
+                    if (res.meta.code !== 200)
                         return this.$message.error(res.meta.message)
+                    window.sessionStorage.setItem("token", res.data.token);
                     this.$message.success(res.meta.message)
+                    this.$router.push('/home')
                 });
             }
         }
@@ -69,11 +72,12 @@
 </script>
 
 <style lang="less" scoped>
-    .login_container{
+    .login_container {
         background-color: #2b4b6b;
         height: 100%;
     }
-    .login_box{
+
+    .login_box {
         width: 450px;
         height: 300px;
         background-color: white;
@@ -81,9 +85,10 @@
         position: absolute;
         left: 50%;
         top: 50%;
-        transform: translate(-50%,-50%);
+        transform: translate(-50%, -50%);
     }
-    .avatar_box{
+
+    .avatar_box {
         height: 130px;
         width: 130px;
         border: 1px solid white;
@@ -93,8 +98,9 @@
         box-shadow: 0 0 10px #ddd;
         position: absolute;
         left: 50%;
-        transform: translate(-50%,-50%);
-        img{
+        transform: translate(-50%, -50%);
+
+        img {
             width: 100%;
             height: 100%;
             border-radius: 50%;
@@ -102,7 +108,7 @@
         }
     }
 
-    .login_form{
+    .login_form {
         position: absolute;
         bottom: 0;
         width: 100%;
@@ -110,7 +116,7 @@
         box-sizing: border-box;
     }
 
-    .btns{
+    .btns {
         display: flex;
         justify-content: flex-end;
     }
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from "@/components/Login";
+import Login from "@/components/Login"
+import Home from "@/components/Home"
 
 Vue.use(VueRouter)
 
 const routes = [
     {path:'/login',component:Login},
-    {path: '/',redirect:'/login'}
+    {path: '/',redirect:'/login'},
+    {path: '/home',component:Home},
 ]
 
 const router = new VueRouter({
-  routes
+    routes
+})
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next)=>{
+    //to:将要访问的路径
+    //from:从哪个路径跳转过来
+    //next:函数，表示放行
+    if(to.path === '/login')
+        return next();
+    //获取token
+    const tokenstr = window.sessionStorage.getItem("token");
+    if(!tokenstr)
+        return next("/login");
+    next();
 })
 
 export default router
